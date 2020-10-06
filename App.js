0f834d907ba9e1eccdd3794714ff0475e3aa4aea
nandan6kaushik <nandan6kaushik@gmail.com>
2020-10-06 10:37:39 +0530

    updating port

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const data = require('./data.json')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const data = require('./data.json');
 
 app.get('/', (req, res) => {
-  res.send(data)
+  res.send(data);
 })
 
 app.listen(port, () => {
-  console.log(`Listening on ${port}`)
+  console.log(`Listening on ${port}`);
 })
