dd0799345d7b404b3118ddb4231b54c76a2b9b21
nkaushikd <nandan6kaushik@gmail.com>
2020-12-01 08:22:36 +0530

    change

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const data = require('./data.json');
-const data2 = require('./data2.json');
+const data2 = require('./data-2.json');
 
 app.get('/', (req, res) => {
   res.send(data);
