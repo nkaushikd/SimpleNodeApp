1e6d9866fed6ea2100596ff0fe06742b93e02387
nkaushikd <nandan6kaushik@gmail.com>
2020-12-01 08:02:30 +0530

    adding data 2

--- App.js
+++ App.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const data = require('./data.json');
+const data2 = require('./data2.json');
 
 app.get('/', (req, res) => {
   res.send(data);
+})
+
+app.get('/horizontal', (req, res) => {
+  res.send(data2);
 })
 
 app.listen(port, () => {
